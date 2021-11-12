81c5aa6fbbb665928ca139fee444ed0b65a3fd86
James Hogan <james@albanarts.com>
2021-11-12 23:53:17 +0000

    Implement FGVRPick VR mode process
    Implement a new VR mode process for picking using a VR controller as a
    pointer, and emulating simple mouse button presses to the pick
    callbacks. The reach is configurable and a simple red laser is shown at
    the correct position which switches to green if pick callbacks are
    found.

--- src/Input/FGVRInput.cxx
+++ src/Input/FGVRInput.cxx
@@ -38,6 +38,7 @@
 #include <cmath>
 
 #include "FGVRButton.hxx"
+#include "FGVRPick.hxx"
 #include "FGVRPoseEuler.hxx"
 
 using flightgear::CameraGroup;
@@ -767,7 +768,8 @@ FGVRInput::Mode::SubactionInfo::~SubactionInfo()
         delete process;
 }
 
-void FGVRInput::Mode::SubactionInfo::readProcesses(Mode* mode,
+void FGVRInput::Mode::SubactionInfo::readProcesses(FGVRInput* input,
+                                                   Mode* mode,
                                                    SGPropertyNode* node,
                                                    SGPropertyNode* statusNode)
 {
@@ -784,6 +786,9 @@ void FGVRInput::Mode::SubactionInfo::readProcesses(Mode* mode,
         if (processType == "button") {
             process = new FGVRButton(mode, _subaction, processNode,
                                      processStatusNode);
+        } else if (processType == "pick") {
+            process = new FGVRPick(input, mode, _subaction, processNode,
+                                   processStatusNode);
         } else if (processType == "pose_euler") {
             process = new FGVRPoseEuler(mode, _subaction, processNode,
                                         processStatusNode);
@@ -901,10 +906,10 @@ FGVRInput::Mode::Mode(FGVRInput* input,
             statusPath << "modes/" << type << "/" << mode << "[" << i << "]";
             SGPropertyNode* statusNode = input->getStatusNode()->getNode(statusPath.str(), true);
             // Read mode wide process objects
-            info->readProcesses(this, nodeCopy, statusNode);
+            info->readProcesses(input, this, nodeCopy, statusNode);
 
             // And subaction specific process objects
-            info->readProcesses(this, subactionCopy, statusNode);
+            info->readProcesses(input, this, subactionCopy, statusNode);
         }
     }
 }
--- src/Input/FGVRInput.hxx
+++ src/Input/FGVRInput.hxx
@@ -914,7 +914,8 @@ public:
             ~SubactionInfo();
 
             /// Read process objects from a given property node.
-            void readProcesses(Mode* mode, SGPropertyNode* node,
+            void readProcesses(FGVRInput* input, Mode* mode,
+                               SGPropertyNode* node,
                                SGPropertyNode* statusNode);
             /// Run initial nasal commands in both nodes.
             void initNasal();
