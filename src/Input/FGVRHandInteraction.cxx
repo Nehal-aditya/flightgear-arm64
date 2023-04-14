3e3d4c5aacdaeec7b7d811919ab88d026a775b57
James Hogan <james@albanarts.com>
2023-04-14 21:52:35 +0100

    Update IK header and class names
    s/SGAnimationIK*/SGIK*/

--- src/Input/FGVRHandInteraction.cxx
+++ src/Input/FGVRHandInteraction.cxx
@@ -25,7 +25,7 @@
 #include <Main/fg_props.hxx>
 #include <Viewer/VRManager.hxx>
 
-#include <simgear/scene/model/SGAnimationIK.hxx>
+#include <simgear/scene/model/SGIKLink.hxx>
 #include <simgear/scene/util/RenderConstants.hxx>
 #include <simgear/scene/util/SGSceneUserData.hxx>
 
@@ -53,7 +53,7 @@ public:
         //struct SGSceneryPick pick;
         osg::observer_ptr<osg::Node> rootNode;
         // FIXME save pointing...
-        SGAnimationIKLink* ikLink;
+        SGIKLink* ikLink;
         std::shared_ptr<SGSpringPickContact> contact;
         // Contact point relative to palm
         osg::Vec3f palmContactPos;
@@ -172,7 +172,7 @@ void FGVRHandInteraction::update(double dt)
     invPalmMat.preMultTranslate(-palmLoc.getPosition());
 
     for (unsigned int grab = 0; grab < 6; ++grab) {
-        SGAnimationIKLink::LinkPath ikLinks;
+        SGIKLink::LinkPath ikLinks;
         int rootIndex = -1;
         osg::Matrix rootMatrix, tipMatrix;
         //std::cout << "Grab " << grab << " = " << grabs[grab] << " (changed: " << grabsChanged[grab] << ")" << std::endl;
