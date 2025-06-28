321162a5d5e9d0651374de1def061a19f8b2aabe
Florent Rougon <f.rougon@frougon.net>
2025-06-28 10:20:40 +0200

    Options::printJSONReport(): fix compilation error (missing brace)

--- src/Main/options.cxx
+++ src/Main/options.cxx
@@ -3402,7 +3402,7 @@ void Options::printJSONReport() const
         {"terrasyncPath", globals->get_terrasync_dir()},
         {"downloadPath", globals->get_download_dir()},
         {"autosavePath", globals->autosaveFilePath()},
-        {"sentryUUID", sentryUserId()};
+        {"sentryUUID", sentryUserId()}};
 
     rootNode["config"]["sceneryPaths"] = globals->get_fg_scenery();
     rootNode["config"]["aircraftPaths"] = globals->get_aircraft_paths();
