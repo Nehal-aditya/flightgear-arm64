271c408daa381926416b6da00d9306cfa81b9e71
James Turner <james@flightgear.org>
2025-07-28 10:54:06 +0100

    src/Network/http/jsonprops.hxx: fix builds without precompiled-headers.

--- src/Network/http/jsonprops.hxx
+++ src/Network/http/jsonprops.hxx
@@ -2,26 +2,13 @@
 //
 // Written by Torsten Dreyer, started April 2014.
 //
-// Copyright (C) 2014  Torsten Dreyer
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// SPDX-FileCopyrightText: Copyright (C) 2014  Torsten Dreyer
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #include <nlohmann/json_fwd.hpp>
-#include <simgear/props/propsfwd.hxx>
+#include <simgear/props/props.hxx>
 #include <string>
 
 namespace flightgear {
