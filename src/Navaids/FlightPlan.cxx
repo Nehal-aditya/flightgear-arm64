b2a20399a8e0704545f88072b3df637de6c6ee13
James Turner <james@flightgear.org>
2025-08-05 14:11:50 +0100

    Fix crash on parsing an ICAO route string

--- src/Navaids/FlightPlan.cxx
+++ src/Navaids/FlightPlan.cxx
@@ -2146,9 +2146,6 @@ bool FlightPlan::parseICAORouteString(const std::string& routeData)
 
     SG_LOG(SG_AUTOPILOT, SG_INFO, "adding waypoints from string");
     // rebuild legs from waypoints we created
-    for (auto l : _legs) {
-        delete l;
-    }
     _legs.clear();
     insertWayptsAtIndex(enroute, 0);
 
