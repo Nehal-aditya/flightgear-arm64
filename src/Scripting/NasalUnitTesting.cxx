f300dd30fcd74b01eeb49331f7a11c5274fc064d
Roman Ludwicki <roman.ludwicki@playerom.com>
2025-10-19 15:03:11 +0200

    Fix `nasal-test-dir` fgcommand

--- src/Scripting/NasalUnitTesting.cxx
+++ src/Scripting/NasalUnitTesting.cxx
@@ -199,9 +199,9 @@ void executeNasalTestsInDir(const SGPath& path)
 {
     simgear::Dir d(path);
 
-    for (const auto& testFile : d.children(simgear::Dir::TYPE_FILE, "*.nut")) {
+    for (const auto& testFile : d.children(simgear::Dir::TYPE_FILE, ".nut")) {
         SG_LOG(SG_NASAL, SG_INFO, "Processing test file " << testFile);
-
+        executeNasalTest(testFile);
     } // of test files iteration
 }
 
