ffc0a19dafde7ed2c3396c5ab6249a025a77f606
Fahim Dalvi <dalvifahim+gitlab@gmail.com>
2025-10-22 08:10:29 +0000

    Fix #3251: Relax JSON serialization
    Nlohmann JSON throws an exception if we try to dump a string that has
    incorrectly encoded UTF-8 sequences, which crashes FG completely. This
    commit relaxes the serialization process and instead replaces the invalid
    UTF-8 sequences with U+FFFD.

--- src/Network/http/jsonprops.cxx
+++ src/Network/http/jsonprops.cxx
@@ -2,21 +2,8 @@
 //
 // Written by Torsten Dreyer, started April 2014.
 //
-// Copyright (C) 2014  Torsten Dreyer
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// SPDX-FileCopyrightText: 2014 Torsten Dreyer
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "jsonprops.hxx"
 #include "simgear/debug/debug_types.h"
@@ -190,9 +177,8 @@ void JSON::addChildrenToProp(const json& j, SGPropertyNode_ptr n)
 string JSON::toJsonString(bool indent, SGPropertyNode_ptr n, int depth, double timestamp )
 {
     auto j = toJson(n, depth, timestamp);
-    return j.dump(indent);
+    return j.dump(indent, ' ', false, nlohmann::detail::error_handler_t::replace);
 }
 
 }  // namespace http
-}  // namespace flightgear
-
+} // namespace flightgear
