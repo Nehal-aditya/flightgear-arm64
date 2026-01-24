Nehal-aditya/flightgear-arm64

commit 364cfd20fb838616d49d268dab7e3339c62f5e79

FGSoundManager: Conditionalise VR head tracked sound
In order to make the head-tracking of surround sound in VR conditional
(since some users may use headphones, others may use fixed speakers),
choose which position and orientation to pass to the sound backend
depending on /sim/sound/head-tracked.

src/Sound/soundmanager.cxx
@@ -59,6 +59,7 @@ void FGSoundManager::init()
     _sound_enabled = fgGetNode("/sim/sound/enabled");
     _volume        = fgGetNode("/sim/sound/volume");
     _device_name   = fgGetNode("/sim/sound/device-name");
+    _headTracked = SGPropObjBool("/sim/sound/head-tracked");
 
     _velocityNorthFPS = fgGetNode("velocities/speed-north-fps", true);
     _velocityEastFPS  = fgGetNode("velocities/speed-east-fps", true);
@@ -163,8 +164,13 @@ void FGSoundManager::update(double dt)
         if (enabled)
         {
             flightgear::View* _view = globals->get_current_view();
-            set_position( _view->getViewPosition(), _view->getPosition() );
-            set_orientation( _view->getViewOrientation() );
+            if (_headTracked) {
+                set_position(_view->getViewPosition(), _view->getPosition());
+                set_orientation(_view->getViewOrientation());
+            } else {
+                set_position(_view->getLocalPosition(), _view->getPosition());
+                set_orientation(_view->getLocalOrientation());
+            }
 
             SGVec3d velocity(SGVec3d::zeros());
             if (!stationaryView()) {

src/Sound/soundmanager.hxx
@@ -8,9 +8,10 @@
 #include <memory>
 #include <map>
 
+#include <simgear/props/propertyObject.hxx>
 #include <simgear/props/props.hxx>
-#include <simgear/structure/subsystem_mgr.hxx>
 #include <simgear/sound/soundmgr.hxx>
+#include <simgear/structure/subsystem_mgr.hxx>
 
 class FGSampleQueue;
 class SGSoundMgr;
@@ -48,6 +49,7 @@ private:
     double _active_dt;
     bool _is_initialized, _enabled;
     SGPropertyNode_ptr _sound_working, _sound_enabled, _volume, _device_name;
+    SGPropObjBool _headTracked;
     SGPropertyNode_ptr _velocityNorthFPS, _velocityEastFPS, _velocityDownFPS;
     SGPropertyNode_ptr _frozen;
     std::unique_ptr<Listener> _listener;

commit aac13cf87672c9c03967fc912fc4d4866096a3ea

Sound Manager: Fix missing head-tracked property
The recently added /sim/vr/head-tracked property isn't yet in
defaults.xml, resulting in PropertyObject exceptions as shown by sentry
(unfortunately its presence in the autosave XML was masking the issue
for me).

Ensure the property exists in FGSoundManager::init(). This will treat it
as defaulting to false, however thats consistent with previous behaviour
and is acceptable until the FGData changes are merged.

Fixes: 364cfd20fb83 ("FGSoundManager: Conditionalise VR head tracked sound")

src/Sound/soundmanager.cxx
@@ -72,6 +72,8 @@ void FGSoundManager::init()
 
     globals->get_commands()->addCommand("play-audio-sample", this, &FGSoundManager::playAudioSampleCommand);
 
+    // ensure PropertyObject properties exist.
+    _headTracked.node(true);
 
     reinit();
 }