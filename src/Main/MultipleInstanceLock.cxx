af71ff72a282fcfb9f00831f4e649cbde804d53c
James Turner <james@flightgear.org>
2026-02-06 15:03:41 +0000

    Forward/setup fixes

--- src/Main/MultipleInstanceLock.cxx
+++ src/Main/MultipleInstanceLock.cxx
@@ -121,8 +121,10 @@ LockStatus acquireLock()
             return LockFailed;
         }
 
-        if (GetLastError() == ERROR_ALREADY_EXISTS) {
-            return LockAlreadyLocked;
+        const auto err = GetLastError();
+        if (err && (err != ERROR_ALREADY_EXISTS)) {
+            SG_LOG(SG_IO, SG_ALERT, "Failed to create exclusive-access mutex: " << GetLastError());
+            return LockFailed;
         }
     }
 
@@ -157,6 +159,8 @@ void releaseLock(SGPath lockPath)
 {
 #if defined(SG_WINDOWS)
     ReleaseMutex(static_fgMultipleInstanceMutex);
+    CloseHandle(static_fgMultipleInstanceMutex);
+    static_fgMultipleInstanceMutex = nullptr;
     SG_LOG(SG_IO, SG_INFO, "Released lock");
 #else
     int err = ::flock(static_lockFileFd, LOCK_UN);
