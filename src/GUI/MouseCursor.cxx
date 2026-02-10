d84f89acaea6a31bdfb80dbe387e998818ec7421
James Hogan <james@albanarts.com>
2026-02-10 15:22:32 +0000

    MouseCursor: Add missing mouse cursors to mapping
    The mouse_cursor_map[] mapping used by cursorFromString() only has a few
    cursors, but we need the cursors used by the Canvas GUI to be accessible
    so we can provide 3D versions of them for the 3D GUI.
    
    Add all the FGMouseCursor cursor types to the mapping, in the same order
    as they are defined. This adds the following cursor strings:
    - "arrow" (was provided as "inherit" alias)
    - "in-out", "up-down" (was provided as "drag-vertical" alias)
    - "left-side", "right-side", "top-side", "bottom-side"
    - "top-left", "top-right", "bottom-left", "bottom-right"
    - "spin-cw", "spin-ccw"
    - "wait"

--- src/GUI/MouseCursor.cxx
+++ src/GUI/MouseCursor.cxx
@@ -232,18 +232,31 @@ typedef struct {
 } MouseCursorMap;
 
 const MouseCursorMap mouse_cursor_map[] = {
-    { "inherit", FGMouseCursor::CURSOR_ARROW },
-    { "crosshair", FGMouseCursor::CURSOR_CROSSHAIR },
-    { "left-right", FGMouseCursor::CURSOR_LEFT_RIGHT },
-    { "hand", FGMouseCursor::CURSOR_HAND },
-    { "closed-hand", FGMouseCursor::CURSOR_CLOSED_HAND },
-    { "text", FGMouseCursor::CURSOR_IBEAM },
+    {"arrow", FGMouseCursor::CURSOR_ARROW},
+    {"hand", FGMouseCursor::CURSOR_HAND},
+    {"closed-hand", FGMouseCursor::CURSOR_CLOSED_HAND},
+    {"crosshair", FGMouseCursor::CURSOR_CROSSHAIR},
+    {"text", FGMouseCursor::CURSOR_IBEAM},
+    {"in-out", FGMouseCursor::CURSOR_IN_OUT},
+    {"left-right", FGMouseCursor::CURSOR_LEFT_RIGHT},
+    {"up-down", FGMouseCursor::CURSOR_UP_DOWN},
+    {"left-side", FGMouseCursor::CURSOR_LEFT_SIDE},
+    {"right-side", FGMouseCursor::CURSOR_RIGHT_SIDE},
+    {"top-side", FGMouseCursor::CURSOR_TOP_SIDE},
+    {"bottom-side", FGMouseCursor::CURSOR_BOTTOM_SIDE},
+    {"top-left", FGMouseCursor::CURSOR_TOP_LEFT},
+    {"top-right", FGMouseCursor::CURSOR_TOP_RIGHT},
+    {"bottom-left", FGMouseCursor::CURSOR_BOTTOM_LEFT},
+    {"bottom-right", FGMouseCursor::CURSOR_BOTTOM_RIGHT},
+    {"spin-cw", FGMouseCursor::CURSOR_SPIN_CW},
+    {"spin-ccw", FGMouseCursor::CURSOR_SPIN_CCW},
+    {"wait", FGMouseCursor::CURSOR_WAIT},
 
-// aliases
-    { "drag-horizontal", FGMouseCursor::CURSOR_LEFT_RIGHT },
-    { "drag-vertical", FGMouseCursor::CURSOR_UP_DOWN },
-    { 0, FGMouseCursor::CURSOR_ARROW }
-};
+    // aliases
+    {"inherit", FGMouseCursor::CURSOR_ARROW},
+    {"drag-horizontal", FGMouseCursor::CURSOR_LEFT_RIGHT},
+    {"drag-vertical", FGMouseCursor::CURSOR_UP_DOWN},
+    {0, FGMouseCursor::CURSOR_ARROW}};
 
 FGMouseCursor::Cursor FGMouseCursor::cursorFromString(const char* cursor_name)
 {
