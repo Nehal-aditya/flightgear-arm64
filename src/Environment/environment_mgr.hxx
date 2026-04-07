5f2a5b9fecd32c3a6322a53a0a45bb8446ed0d1c
Erik Hofman <erik@ehofman.com>
2026-04-07 13:54:45 +0200

    Fix: Conditional jump or move depends on uninitialised value

--- src/Environment/environment_mgr.hxx
+++ src/Environment/environment_mgr.hxx
@@ -82,7 +82,7 @@ private:
     int max_tower_height_feet;
     int min_tower_height_feet;
     int default_tower_height_feet;
-    bool rebuildCloudLayers;
+    bool rebuildCloudLayers = true;
 
     simgear::TiedPropertyList _tiedProperties;
     SGPropertyChangeListener* _3dCloudsEnableListener;
